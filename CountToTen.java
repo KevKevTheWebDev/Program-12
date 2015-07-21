7164ac6f36363522a9b784d16e25a597d998da38
KevKevTheWebDev <student.122454@worc.k12.ma.us>
2015-07-21 15:50:27 -0400

    Update and rename README.md to CountToTen.java

---
+++ CountToTen.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class apples{
+	public static void main(String args[]){
+		int counter = 0;
+		while (counter < 10){
+			System.out.println(counter);
+			counter++;
+		}
+	}
+}
